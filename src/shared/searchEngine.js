a26e0eb313e5a41b3303a5601ec3c211362af640
Kelly Huang <sissihkl0603@gmail.com>
2019-04-30 22:31:42 +0800

    fix the inconsistent problem

--- src/shared/searchEngine.js
+++ src/shared/searchEngine.js
@@ -45,7 +45,6 @@ let N; // num_documnents
 let docs;
 let rank;
 let top_id_list;
-let final_list = [];
 let top_N = 50;
 // var query = ["research", "step", "in"];
 let startTime = Date.now();
@@ -252,6 +251,7 @@ export function searchEngine(query_raw) {
   // function searchEngine(query_raw) {
   console.log("query_raw: ", query_raw);
   let score = [];
+  let final_list = [];
   let top_doc_list = [];
   let query_list = query_raw.split('"');
   let phrase_list = [];
@@ -340,7 +340,7 @@ export function searchEngine(query_raw) {
       }
 
       console.log("TIMER: Printing score ", Date.now() - startTime);
-      // console.log("score: ", score);
+      console.log("score: ", score);
 
       let id_score = [];
       for (let i = 0; i < N; i++) {
@@ -375,7 +375,7 @@ export function searchEngine(query_raw) {
       final_list.sort(function(a, b) {
         return b[2] - a[2];
       });
-      console.log(final_list);
+      console.log("final_list: ", final_list);
 
       let top = Math.min(top_N, N);
       let top_final_list = final_list.slice(0, top);
